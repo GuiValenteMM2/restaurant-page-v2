830e9b6ae9321cec86a66c4f8295ddd82dc02685
GuiValenteMM2 <guimartins.m.v@gmail.com>
2023-12-05 13:54:58 -0300

    Header template moved to a header component, header style done

--- restaurant-page/src/App.vue
+++ restaurant-page/src/App.vue
@@ -1,33 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
 import './assets/main.css'
+import HeaderComp from './components/Header.vue'
 
 </script>
 
 <template>
-  <header class="container bg-yellow-950 flex min-h-screen">
-    <div class="wrapper content-normal flex">
-      <div class="heading relative">
-        <img src="./assets/images/fogaoalenha2.jpg" alt="forno" class="min-h-screen">
-        <h1 class="heading-text object-center z-10 text-white text-5xl absolute w-full bg-yellow-950/60 top-1/3">Forno De Casa</h1>
-      </div>
-      <div class="content flex">
-        <nav class= "space-x-5 text-xl text-cyan-50 divide-x-1 h-28">
-          <RouterLink to="/cart"
-          >Carrinho</RouterLink>
-          <RouterLink to="/"
-          >Cardápio</RouterLink>
-          <RouterLink to="/about"
-          >Sobre nós</RouterLink>
-          <RouterLink to="/signup"
-          >Cadastre-se</RouterLink>
-          <RouterLink to="/contact"
-          >Contate-nos</RouterLink>
-        </nav>
-            </div>
-            <RouterView />
-      </div>
-  </header>
+  <HeaderComp />
 </template>
 
 <style scoped>
---
+++ restaurant-page/src/components/Header.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header class="container bg-yellow-950 min-h-screen">
+    <div class="wrapper content-normal flex gap-4">
+      <div class="heading relative">
+        <img src="../assets/images/fogaoalenha2.jpg" alt="forno" class="min-h-screen">
+        <h1 class="heading-text object-center z-10 text-white text-5xl absolute w-full bg-yellow-950/60 top-1/3">Forno De Casa</h1>
+      </div>
+      <div class="content flex flex-col">
+        <nav class= "space-x-5 text-xl text-cyan-50 divide-x-1 h-28 p-4">
+          <RouterLink class="focus:text-yellow-700 " to="/cart"
+          >Carrinho</RouterLink>
+          <RouterLink class="focus:text-yellow-700" to="/menu"
+          >Cardápio</RouterLink>
+          <RouterLink class="focus:text-yellow-700" to="/"
+          >Sobre nós</RouterLink>
+          <RouterLink class="focus:text-yellow-700" to="/signup"
+          >Cadastre-se</RouterLink>
+          <RouterLink class="focus:text-yellow-700" to="/contact"
+          >Contate-nos</RouterLink>
+        </nav>
+        <RouterView />
+            </div>
+      </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
